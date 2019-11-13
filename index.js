9206d421d7a7aa3d9842681ab61716dec2420bfd
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2019-11-13 15:59:27 -0500

    working on project

--- index.js
+++ index.js
@@ -39,8 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age,
+  this.stomach = []
 
+}
+
+Person.prototype.poop = function(){
+  this.stomach = []
+}
+Person.prototype.eat = function(currentValue){
+  this.stomach.push(this.currentValue){
+  if stomach.length>=10{
+    this.poop()
+
+  }
+  return this.stomach
+}
+Person.prototype.toString = function(){
+  return '${this.name},${this.age}'
 }
 
 /*
